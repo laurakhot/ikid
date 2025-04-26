23d4e226d5e386c3f29e1d1b9b2d564894d6211e
laurakhot <laurak_11@uw.edu      git config --global user.email laurak_11@uw.edu>
2025-04-26 16:22:11 -0700

    Initial Commit

---
+++ ikid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ikid
+//
+//  Created by Laura Khotemlyansky on 4/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
